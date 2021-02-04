d6cd2b7f0385eccd55a97bbd875ebc2000596c22
Cinnamonsroll <71993095+Cinnamonsroll@users.noreply.github.com>
2021-02-04 01:03:39 -0500

    Create disable.js
    get rekt

---
+++ commands/disable.js
@@ -0,0 +1,7 @@
+const Discord = require("discord.js");module.exports.run=async(client,message,args)=>{if(!message.member.permissions.has("MANAGE_GUILD"))return message.channel.send("You're bad");let db=require("quick.db");if(!args[0])return message.channel.send("INCLUDE A FUCKING OPTION EITHER ENABLE OR DISABLE");let command=args[1];if(!command)return message.channel.send("Please include a command name");command=command.toLowerCase();if(!client.commands.get(command))return message.channel.send("That is not a valid command");let state=db.fetch(`${message.guild.id}_${command}`);if(state&&state==="disabled")return message.channel.send("This command is already disabled");db.set(`${message.guild.id}_${command}`, "disabled");message.channel.send(`Disabled the command ${command}`);};
+module.exports.help = {
+  name: "disable",
+  description: "Disables any command",
+  usage: " [command]",
+  aliases: ["getdisownedcommand"]
+};
